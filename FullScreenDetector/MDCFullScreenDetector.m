a65cbe3f7a1d46af2a54fe2fabe4570aadc7cf1e
Mark Christian <m@rkchristian.ca>
2014-03-22 13:56:29 -0700

    Fix compile fail

--- FullScreenDetector/MDCFullScreenDetector.m
+++ FullScreenDetector/MDCFullScreenDetector.m
@@ -17,9 +17,9 @@ NSString * kMDCFullScreenDetectorSwitchedToRegularSpace = @"com.whimsicalifornia
 - (id)init {
 
   //  Make sure the application doesn't have LSUIElement set to YES; if so, full screen detection won't work, and we should log a warning.
-  NSNumber *uiElement = [NSBundle mainBundle].infoDictionary[@"LSUIElement"];
+  NSNumber *uiElement = [NSBundle.mainBundle.infoDictionary objectForKey:@"LSUIElement"];
   if (uiElement.boolValue) {
-    NSLog(@"Warning to developer of %@: full screen detection does not work when LSUIElement is set to YES in Info.plist.", [NSBundle mainBundle].infoDictionary[@"CFBundleName"]);
+    NSLog(@"Warning to developer of %@: full screen detection does not work when LSUIElement is set to YES in Info.plist.", [NSBundle.mainBundle.infoDictionary objectForKey:@"CFBundleName"]);
   }
 
   NSInteger styleMask = NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask;
